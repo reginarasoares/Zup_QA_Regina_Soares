5a52551904ddbfe29e0305f8b774009add3caff6
regina_soares <reginarasoares@gmail.com>
2020-03-28 16:22:41 -0300

    Cenario busca incorreta

--- features/specs/compra.feature
+++ features/specs/compra.feature
@@ -7,11 +7,23 @@ Contexto:
 Dado eu estar na pagina inicial do site
 
 
+@end_to_end
 Esquema do Cenario: Buscar e adiocionar produto no carrinho com sucesso
-Quando eu faco uma busca valida por um <produto>
+Quando eu faco uma busca por um <produto>
 E escolho um <determinado> produto
 E adiciono o produto no carrinho
-Entao eu verifico verifico se o produto foi adicionado corretamente
+Entao eu verifico se o produto foi adicionado corretamente
 Exemplos:
 |                        produto                          |     determinado       |
 |  "Alcool Isopropilico 250ml Com Bico Aplicador 1litro"  |            0          |
+
+
+
+@busca_incorreta
+Esquema do Cenario: Preencher o campo de busca incorretamente
+Quando eu faco uma busca por <algo> que nao seja um produto
+Entao eu verifico se aparece a mensagem que nenhum produto foi encontrado
+Exemplos:
+|   algo   |
+| "123456" |
+|  "////"  |
--- features/step_definitions/compra.rb
+++ features/step_definitions/compra.rb
@@ -3,7 +3,7 @@ Dado("eu estar na pagina inicial do site") do
     windows.last.maximize
 end
 
-Quando("eu faco uma busca valida por um {string}") do |name_produto|
+Quando("eu faco uma busca por um {string}") do |name_produto|
     fill_in(name: "conteudo", with: name_produto)
     sleep(2)
     find("#h_search-btn").click
@@ -27,9 +27,24 @@ Quando("adiciono o produto no carrinho") do
     end
 end
   
-Entao("eu verifico verifico se o produto foi adicionado corretamente") do
+Entao("eu verifico se o produto foi adicionado corretamente") do
     puts @name_opcao
     escolha = find(".basket-productTitle").text.downcase
     expect(escolha). to eql @name_opcao
     puts escolha
+end
+
+
+
+
+Quando("eu faco uma busca por {string} que nao seja um produto") do |busca_incorreta|
+    fill_in(name: "conteudo", with: busca_incorreta)
+    sleep(2)
+    find("#h_search-btn").click
+    sleep(2)
+end
+  
+Entao("eu verifico se aparece a mensagem que nenhum produto foi encontrado") do
+    msg = find(".TextUI-sc-12tokcy-0.goIRTq").text
+    expect(msg). to eql "Ops!"
 end
